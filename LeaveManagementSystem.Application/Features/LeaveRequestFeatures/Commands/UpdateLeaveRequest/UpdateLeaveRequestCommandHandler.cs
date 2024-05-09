da347aea113e44eb334ef57ed9bb23ba3500e466
KEMARNORTON <kemarnorton@hotmail.com>
2024-05-08 21:11:19 -0500

    add update commands for leave request

--- LeaveManagementSystem.Application/Features/LeaveRequestFeatures/Commands/UpdateLeaveRequest/UpdateLeaveRequestCommandHandler.cs
+++ LeaveManagementSystem.Application/Features/LeaveRequestFeatures/Commands/UpdateLeaveRequest/UpdateLeaveRequestCommandHandler.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using LeaveManagementSystem.Application.Contracts.EmailService;
+using LeaveManagementSystem.Application.Contracts.ILogging;
 using LeaveManagementSystem.Application.Contracts.Persistance;
 using LeaveManagementSystem.Application.Exceptions;
+using LeaveManagementSystem.Application.Models.Email;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -17,13 +19,15 @@ namespace LeaveManagementSystem.Application.Features.LeaveRequestFeatures.Comman
         private readonly ILeaveTypeRepository _leaveTypeRepository;
         private readonly IEmailService _emailService;
         private readonly IMapper _mapper;
+        private readonly IAppLogger<UpdateLeaveRequestCommandHandler> _appLogger;
 
-        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, ILeaveTypeRepository leaveTypeRepository, IEmailService emailService, IMapper mapper)
+        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, ILeaveTypeRepository leaveTypeRepository, IEmailService emailService, IMapper mapper, IAppLogger<UpdateLeaveRequestCommandHandler> appLogger)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _leaveTypeRepository = leaveTypeRepository;
             _emailService = emailService;
             _mapper = mapper;
+            _appLogger = appLogger;
         }
 
         public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
@@ -35,7 +39,38 @@ namespace LeaveManagementSystem.Application.Features.LeaveRequestFeatures.Comman
                 throw new NotFoundException(nameof(leaveRequest), request.Id);
             }
 
+            var validator = new UpdateLeaveRequestCommandValidator(_leaveTypeRepository, _leaveRequestRepository);
+            var validationResult = await validator.ValidateAsync(request);
 
+            if (validationResult.Errors.Any())
+            {
+                throw new BadRequestException("Invalid Leave Request", validationResult);
+            }
+
+            _mapper.Map(request, leaveRequest);
+
+            await _leaveRequestRepository.UpdateAsync(leaveRequest);
+
+            try
+            {
+                var email = new EmailMessage
+                {
+                    To = string.Empty,
+                    Body = $"Your leave request for {request.StartDate} to {request.EndDate} has been updated sucessfully.",
+                    Subject = "Leave Request Submission"
+                };
+
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex) 
+            {
+                _appLogger.LogWarning(ex.Message);
+
+            }
+
+            
+
+            return Unit.Value;
         }
     }
 }
